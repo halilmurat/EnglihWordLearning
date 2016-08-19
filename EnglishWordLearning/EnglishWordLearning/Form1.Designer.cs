8602213575dd7c5bd4622c2d01a3cfe1b67d9127
halilmurat <halilmurat14@hotmail.com>
2016-08-19 19:30:22 +0300

    x
    x

---
+++ EnglishWordLearning/EnglishWordLearning/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace EnglishWordLearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(0, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "connection status";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(377, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(187, 264);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(135, 221);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(46, 37);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "Start";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(6, 235);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "<---";
+            this.toolTip1.SetToolTip(this.button8, "Only write Id number");
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(39, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Id :";
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Location = new System.Drawing.Point(106, 45);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(31, 27);
+            this.button6.TabIndex = 13;
+            this.toolTip1.SetToolTip(this.button6, "Change");
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Remove";
+            this.toolTip1.SetToolTip(this.button3, "Only write Id number");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Turkish :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "English :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(86, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TopMost";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 264);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(130, 217);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 30);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Reset Data";
+            this.toolTip1.SetToolTip(this.button7, "Delete all data from database");
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(74, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add";
+            this.toolTip1.SetToolTip(this.button2, "Add words to your database");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(220, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Bring";
+            this.toolTip1.SetToolTip(this.button4, "Shown your words on the board.");
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 166);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(147, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Update";
+            this.toolTip1.SetToolTip(this.button5, "You must write Id of which you want update word.");
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(76, 162);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(564, 281);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "EWL";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+    }
+}
+
---
+++ EnglishWordLearning/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EnglishWordLearning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Event();
+            Bring();
+            ShowFormDisable();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=ENJOYKOVBOY;Initial Catalog=mydb;Integrated Security=True");
+        SqlCommand comm = new SqlCommand();
+        int a = 0;
+
+        public void Event()
+        {
+            if(conn.State == ConnectionState.Open)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = "connection open";
+            }
+
+            else if(conn.State == ConnectionState.Closed)
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "connection close";
+            }
+
+            else if(conn.State == ConnectionState.Executing)
+            {
+                label1.ForeColor = Color.Blue;
+                label1.Text = "processing on";
+            }
+        }
+
+        public void Add(string eng, string tr)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "")
+            {
+                textBox1.Focus();
+                if(textBox1.Text != "") textBox2.Focus();
+            }
+            else
+            {
+                conn.Open();
+                Event();
+                comm.Connection = conn;
+                comm.CommandText = "insert into EnglishWordLearning values (@eng, @tur)";
+                comm.Parameters.AddWithValue("@eng", eng);
+                comm.Parameters.AddWithValue("@tur", tr);
+                if(comm.ExecuteNonQuery() == 1)
+                    MessageBox.Show("Your words on the server at now");
+
+                comm.Parameters.Clear();
+                conn.Close();
+                Event();
+            }
+        }
+
+        public void Bring()
+        {
+            conn.Open();
+            Event();
+            DataSet ds = new DataSet();
+            SqlDataAdapter adaptr = new SqlDataAdapter("select * from EnglishWordLearning", conn);
+            adaptr.Fill(ds, "EnglishWordLearning");
+            dataGridView1.DataSource = ds.Tables["EnglishWordLearning"];
+            comm.Parameters.Clear();
+            conn.Close();
+            Event();
+        }
+
+        public void Update(string eng, string tur, int Id)
+        {
+            conn.Open();
+            Event();
+            comm.Connection = conn;
+            comm.CommandText = "update EnglishWordLearning set english = @Eng, Turkish = @Tur Where Id='" + Convert.ToInt32(numericUpDown1.Text) + "'";
+            comm.Parameters.AddWithValue("@Eng", eng);
+            comm.Parameters.AddWithValue("@Tur", tur);
+            if(comm.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Updated was succesfully");
+            }
+            else
+            {
+                MessageBox.Show("another");
+            }
+            comm.Parameters.Clear();
+            conn.Close();
+            Event();
+        }
+
+        public void Remove()
+        {
+            conn.Open();
+            Event();
+            comm.Connection = conn;
+            comm.CommandText = "delete from EnglishWordLearning where Id='" + numericUpDown1.Text + "'";
+            comm.ExecuteNonQuery();
+            comm.Parameters.Clear();
+            conn.Close();
+            Event();
+        }
+
+        public void ShowFormEnable()
+        {
+            groupBox2.Visible = true;
+            this.Height = 320;
+            this.Width = 580;
+            this.Location = new Point(780, 70);
+            button8.Text = "-->";
+        }
+
+        public void ShowFormDisable()
+        {
+            groupBox2.Visible = false;
+            this.Height = 320;
+            this.Width = 200;
+            this.Location = new Point(1150, 70);
+            button8.Text = "<--";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.TopMost == true) this.TopMost = false;
+            else this.TopMost = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Add(textBox1.Text, textBox2.Text);
+            Bring();
+            Event();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Remove();
+            Bring();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Text = "refresh";
+            Bring();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Update(textBox1.Text, textBox2.Text, Convert.ToInt32(numericUpDown1.Text));
+            Bring();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = a;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            comm.Connection = conn;
+            comm.CommandText = "Delete From EnglishWordLearning";
+            comm.ExecuteNonQuery();
+            conn.Close();
+            Bring();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(groupBox2.Visible == false)
+            {
+                ShowFormEnable();
+            }
+            else
+            {
+                ShowFormDisable();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == false)
+            {
+                button9.Text = "Stop";
+                button8.Enabled = false;
+                button3.Enabled = false;
+                timer1.Enabled = true;
+                groupBox2.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button9.Text = "Start";
+                button8.Enabled = true;
+                button9.Enabled = true;
+                groupBox2.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a++;
+            button9.Text = a.ToString();
+            if(a == 10)
+            {
+                ShowFormEnable();
+            }
+            if(a == 13)
+            {
+                ShowFormDisable();
+                a = 0;
+            }
+        }
+    }
+}
